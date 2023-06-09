6d51252abc051d27a7841d69a7bdcdcde5031c3c
Bohdan <bohdanvoroniak455@gmail.com>
2023-06-09 21:51:34 +0300

    delete useless project

--- Test/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using TelegramBotLibrary;
-
-var bot = new TelegramBotClient("5936336198:AAFQ2RS5auQ0p6aSmM2qlRTaxmdRmsi_Jr8");
-await bot.SendMessageAsync(561013085, "Hello");
-var result = await bot.GetMeAsync();
-Console.WriteLine(result.FirstName);
